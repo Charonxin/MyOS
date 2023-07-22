70b785cb1442a057a78de3dd94e2b33bde215452
charonxin <1656746727@qq.com>
2023-07-23 00:12:45 +0800

    update interrupt program

--- kernel/main.c
+++ kernel/main.c
@@ -1,5 +1,12 @@
-int 
-main(void) {
-	while(1);
-	return 0;
+# include "kernel/print.h"
+# include "init.h"
+
+void main(void) {
+    put_str("I am kernel.\n");
+    init_all();
+    put_str("Init finished.\n");
+    asm volatile ("sti");
+    put_str("Turn on the interrupt.\n");
+    while (1);
 }
+
