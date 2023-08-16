8b7ed90a1f34a835aa802ad8f3291124115a2e46
charonxin <1656746727@qq.com>
2023-08-16 19:03:53 +0800

    12_3 syscall

--- lib/user/syscall.c
+++ lib/user/syscall.c
@@ -4,45 +4,52 @@
 #define _syscall0(NUMBER) ({				       \
    int retval;					               \
    asm volatile (					       \
-   "int $0x80"						       \
+   "pushl %[number]; int $0x80; addl $4, %%esp"		       \
    : "=a" (retval)					       \
-   : "a" (NUMBER)					       \
+   : [number] "i" (NUMBER)		  		       \
    : "memory"						       \
    );							       \
    retval;						       \
 })
 
 /* 一个参数的系统调用 */
-#define _syscall1(NUMBER, ARG1) ({			       \
+#define _syscall1(NUMBER, ARG0) ({			       \
    int retval;					               \
    asm volatile (					       \
-   "int $0x80"						       \
+   "pushl %[arg0]; pushl %[number]; int $0x80; addl $8, %%esp" \
    : "=a" (retval)					       \
-   : "a" (NUMBER), "b" (ARG1)				       \
+   : [number] "i" (NUMBER), [arg0] "g" (ARG0)		       \
    : "memory"						       \
    );							       \
    retval;						       \
 })
 
 /* 两个参数的系统调用 */
-#define _syscall2(NUMBER, ARG1, ARG2) ({		       \
+#define _syscall2(NUMBER, ARG0, ARG1) ({		       \
    int retval;						       \
    asm volatile (					       \
-   "int $0x80"						       \
-   : "=a" (retval)					       \
-   : "a" (NUMBER), "b" (ARG1), "c" (ARG2)		       \
-   : "memory"						       \
+   "pushl %[arg1]; pushl %[arg0]; "			       \
+   "pushl %[number]; int $0x80; addl $12, %%esp"	       \
+      : "=a" (retval)					       \
+      : [number] "i" (NUMBER),				       \
+	[arg0] "g" (ARG0),				       \
+	[arg1] "g" (ARG1)				       \
+      : "memory"					       \
    );							       \
    retval;						       \
 })
 
 /* 三个参数的系统调用 */
-#define _syscall3(NUMBER, ARG1, ARG2, ARG3) ({		       \
+#define _syscall3(NUMBER, ARG0, ARG1, ARG2) ({		       \
    int retval;						       \
    asm volatile (					       \
-      "int $0x80"					       \
+      "pushl %[arg2]; pushl %[arg1]; pushl %[arg0]; "	       \
+      "pushl %[number]; int $0x80; addl $16, %%esp"	       \
       : "=a" (retval)					       \
-      : "a" (NUMBER), "b" (ARG1), "c" (ARG2), "d" (ARG3)       \
+      : [number] "i" (NUMBER),				       \
+	[arg0] "g" (ARG0),				       \
+	[arg1] "g" (ARG1),				       \
+	[arg2] "g" (ARG2)				       \
       : "memory"					       \
    );							       \
    retval;						       \
@@ -52,4 +59,3 @@
 uint32_t getpid() {
    return _syscall0(SYS_GETPID);
 }
-
